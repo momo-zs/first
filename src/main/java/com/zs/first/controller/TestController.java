56bd3f699f8264fb5321aa2d130cb0f287909743
zhongsheng <1653389024@qq.com>
2019-11-20 17:37:44 +0800

    整合freemarker

---
+++ src/main/java/com/zs/first/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.zs.first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @date 2019/11/20 0020 14:38
+ * @author zs
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(ModelMap map){
+        map.addAttribute("name","喵喵");
+        map.put("sex", 1);
+        List<String> userList = new ArrayList<String>();
+        userList.add("张三");
+        userList.add("李四");
+        userList.add("王五");
+        map.addAttribute("userList",userList);
+        return "test";
+    }
+}
